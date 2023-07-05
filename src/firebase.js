1ba45a79cf7824fb466e53ac555536daaab6bc8b
Júlia Valls Moix <juliavalls20@gmail.com>
2023-07-05 16:29:18 +0200

    Starting Banner

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAkHB4jzc4NQLBqY4KWKEUI8GsK98RJuI8",
+    authDomain: "netflix-clone-891dc.firebaseapp.com",
+    projectId: "netflix-clone-891dc",
+    storageBucket: "netflix-clone-891dc.appspot.com",
+    messagingSenderId: "506479862282",
+    appId: "1:506479862282:web:1bf1d0fafe7c42549935f3"
+  };
